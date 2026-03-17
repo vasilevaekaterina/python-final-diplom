51eab26097a912ef6beb5b861b42dc17ece3f398
Vasileva Ekaterina <vasilevaekaterina2408@yandex.ru>
2026-03-17 22:02:29 +0300

    password reset integration  with django-rest-passwordreset

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from django_rest_passwordreset.views import (
+    reset_password_confirm,
+    reset_password_request_token,
+)
 
 from backend.views import (
     AccountDetails,
@@ -14,4 +18,10 @@ urlpatterns = [
     path('user/confirm', ConfirmAccount.as_view(), name='user-confirm'),
     path('user/login', LoginAccount.as_view(), name='user-login'),
     path('user/details', AccountDetails.as_view(), name='user-details'),
+    path('user/password_reset', reset_password_request_token, name='password-reset'),
+    path(
+        'user/password_reset/confirm',
+        reset_password_confirm,
+        name='password-reset-confirm',
+    ),
 ]
