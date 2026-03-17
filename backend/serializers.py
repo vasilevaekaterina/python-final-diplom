aee06842051b3850a3021e1a136d0e2afedd6a1c
Vasileva Ekaterina <vasilevaekaterina2408@yandex.ru>
2026-03-17 22:37:08 +0300

    added catalog serializers

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 
 from backend.models import (
+    Category,
     Contact,
+    Product,
+    ProductInfo,
+    ProductParameter,
+    Shop,
     User,
 )
 
@@ -39,5 +44,54 @@ class UserSerializer(serializers.ModelSerializer):
             'company',
             'position',
             'contacts',
+        )
+        read_only_fields = ('id',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shop
+        fields = ('id', 'name', 'state')
+        read_only_fields = ('id',)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+
+    class Meta:
+        model = Product
+        fields = ('name', 'category')
+
+
+class ProductParameterSerializer(serializers.ModelSerializer):
+    parameter = serializers.StringRelatedField()
+
+    class Meta:
+        model = ProductParameter
+        fields = ('parameter', 'value')
+
+
+class ProductInfoSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_parameters = ProductParameterSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = ProductInfo
+        fields = (
+            'id',
+            'model',
+            'product',
+            'shop',
+            'quantity',
+            'price',
+            'price_rrc',
+            'product_parameters',
         )
         read_only_fields = ('id',)
